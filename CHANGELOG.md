1bbd911b126aea2a99e806b932049918744b1de0
José Valim <jose.valim@dashbit.co>
2021-07-18 21:28:40 +0200

    Release v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## v0.1.2 (2021-07-18)
+
+  * Improve docs and error messages
+
 ## v0.1.1 (2021-07-18)
 
   * Fix target detection on ARM Macs and OTP < 24
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Esbuild.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   @source_url "https://github.com/phoenixframework/esbuild"
 
   def project do
