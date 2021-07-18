d181c50415f8652c478418b64f0b34c08a4d5fdc
Wojtek Mach <wojtek@wojtekmach.pl>
2021-07-18 13:37:10 +0200

    Release v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## v0.1.1 (2021-07-18)
+
+  * Fix target detection on ARM Macs and OTP < 24
+
 ## v0.1.0 (2021-07-18)
 
   * First release
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Esbuild.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/phoenixframework/phoenix"
 
   def project do
