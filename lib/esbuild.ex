a50a2ae3a974bc08192ce3d08e3befa38d76ef21
Pablo Dávila <25826704+pablodavila95@users.noreply.github.com>
2021-07-21 10:22:08 -0500

    Modify install/0 so it correctly works with Windows systems (#7)

--- lib/esbuild.ex
+++ lib/esbuild.ex
@@ -164,7 +164,14 @@ defmodule Esbuild do
     end
 
     bin_path = Esbuild.bin_path()
-    File.cp!(Path.join([tmp_dir, "package", "bin", "esbuild"]), bin_path)
+
+    case :os.type() do
+      {:win32, _} ->
+        File.cp!(Path.join([tmp_dir, "package", "esbuild.exe"]), bin_path)
+
+      _ ->
+        File.cp!(Path.join([tmp_dir, "package", "bin", "esbuild"]), bin_path)
+    end
   end
 
   # Available targets: https://github.com/evanw/esbuild/tree/master/npm
