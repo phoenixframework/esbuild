9779f2bc445dc2ae8413e0345bd35050f6cb9872
Dennis Beatty <dennis@dcbeatty.com>
2021-08-31 01:34:11 -0600

    Add --no-runtime-config flag (#19)
    When building assets as part of a CI pipeline, oftentimes environment
    variables that are required by runtime configuration won't be in place
    yet. This adds a new `--no-runtime-config` flag to bypass the runtime
    configuration check and just use the values given as arguments.
    
    Resolves #18

--- lib/mix/tasks/esbuild.ex
+++ lib/mix/tasks/esbuild.ex
@@ -5,7 +5,7 @@ defmodule Mix.Tasks.Esbuild do
   Usage:
 
   ```bash
-  $ mix esbuild CONTEXT ARGS
+  $ mix esbuild TASK_OPTIONS PROFILE ESBUILD_ARGS
   ```
 
   Example:
@@ -17,6 +17,13 @@ defmodule Mix.Tasks.Esbuild do
   If esbuild is not installed, it is automatically downloaded.
   Note the arguments given to this task will be appended
   to any configured arguments.
+
+  Flags to control this Mix task must be given before the
+  profile:
+
+  ```bash
+  $ mix esbuild --no-runtime-config default assets/js/app.js
+  ```
   """
 
   @shortdoc "Invokes esbuild with the profile and args"
@@ -24,20 +31,26 @@ defmodule Mix.Tasks.Esbuild do
   use Mix.Task
 
   @impl true
-  def run([profile | args] = all) do
-    if Code.ensure_loaded?(Mix.Tasks.App.Config) do
+  def run(args) do
+    switches = [runtime_config: :boolean]
+    {opts, remaining_args} = OptionParser.parse_head!(args, switches: switches)
+
+    if Keyword.get(opts, :runtime_config, true) and Code.ensure_loaded?(Mix.Tasks.App.Config) do
       Mix.Task.run("app.config")
     end
 
+    Mix.Task.reenable("esbuild")
+    install_and_run(remaining_args)
+  end
+
+  defp install_and_run([profile | args] = all) do
     case Esbuild.install_and_run(String.to_atom(profile), args) do
       0 -> :ok
       status -> Mix.raise("`mix esbuild #{Enum.join(all, " ")}` exited with #{status}")
     end
-
-    Mix.Task.reenable("esbuild")
   end
 
-  def run([]) do
+  defp install_and_run([]) do
     Mix.raise("`mix esbuild` expects the profile as argument")
   end
 end
