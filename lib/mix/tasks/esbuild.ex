65e7fb15927acef3985c602a4be7b91d5fbd9985
Dennis Beatty <dennis@dcbeatty.com>
2021-08-30 20:47:08 -0600

    add --no-runtime-config flag
    When building assets as part of a CI pipeline, oftentimes environment
    variables that are required by runtime configuration won't be in place
    yet. This adds a new `--no-runtime-config` flag to bypass the runtime
    configuration check and just use the values given as arguments.

--- lib/mix/tasks/esbuild.ex
+++ lib/mix/tasks/esbuild.ex
@@ -5,13 +5,13 @@ defmodule Mix.Tasks.Esbuild do
   Usage:
 
   ```bash
-  $ mix esbuild CONTEXT ARGS
+  $ mix esbuild TASK_OPTIONS CONTEXT ARGS
   ```
 
   Example:
 
   ```bash
-  $ mix esbuild default assets/js/app.js --bundle --minify --target=es2016 --outdir=priv/static/assets
+  $ mix esbuild --no-runtime-config default assets/js/app.js --bundle --minify --target=es2016 --outdir=priv/static/assets
   ```
 
   If esbuild is not installed, it is automatically downloaded.
@@ -24,11 +24,18 @@ defmodule Mix.Tasks.Esbuild do
   use Mix.Task
 
   @impl true
-  def run([profile | args] = all) do
-    if Code.ensure_loaded?(Mix.Tasks.App.Config) do
+  def run(args) do
+    options = [switches: [no_runtime_config: :boolean]]
+    {parsed, remaining_args, _errors} = OptionParser.parse_head(args, options)
+
+    if !parsed[:no_runtime_config] && Code.ensure_loaded?(Mix.Tasks.App.Config) do
       Mix.Task.run("app.config")
     end
 
+    install_and_run(remaining_args)
+  end
+
+  defp install_and_run([profile | args] = all) do
     case Esbuild.install_and_run(String.to_atom(profile), args) do
       0 -> :ok
       status -> Mix.raise("`mix esbuild #{Enum.join(all, " ")}` exited with #{status}")
@@ -37,7 +44,7 @@ defmodule Mix.Tasks.Esbuild do
     Mix.Task.reenable("esbuild")
   end
 
-  def run([]) do
+  defp install_and_run([]) do
     Mix.raise("`mix esbuild` expects the profile as argument")
   end
 end
