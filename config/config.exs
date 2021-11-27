311e7c263212e69351526e247ae76cdf8e47515a
Chris Wögi <789@webuhu.at>
2021-11-27 18:54:09 +0100

    Update esbuild to 0.14.0 (#36)

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,7 @@
 import Config
 
 config :esbuild,
-  version: "0.13.10",
+  version: "0.14.0",
   another: [
     args: ["--version"]
   ]
--- lib/esbuild.ex
+++ lib/esbuild.ex
@@ -1,6 +1,6 @@
 defmodule Esbuild do
   # https://registry.npmjs.org/esbuild/latest
-  @latest_version "0.13.10"
+  @latest_version "0.14.0"
 
   @moduledoc """
   Esbuild is an installer and runner for [esbuild](https://esbuild.github.io).
