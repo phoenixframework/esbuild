749fbe6c0d5ded2cfbaa6af65e71a6e2664cc85c
Sam Hunter <swilkinsonhunter@gmail.com>
2022-05-27 11:09:16 -0700

    Bump Project Version, Esbuild Version For New Release (#51)

--- lib/esbuild.ex
+++ lib/esbuild.ex
@@ -1,6 +1,6 @@
 defmodule Esbuild do
   # https://registry.npmjs.org/esbuild/latest
-  @latest_version "0.14.29"
+  @latest_version "0.14.41"
 
   @moduledoc """
   Esbuild is an installer and runner for [esbuild](https://esbuild.github.io).
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Esbuild.MixProject do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.5.0"
   @source_url "https://github.com/phoenixframework/esbuild"
 
   def project do
