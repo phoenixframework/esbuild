94d18da850927512ac8de7f4a201f3407a225527
Gonzalo <456459+grzuy@users.noreply.github.com>
2023-10-10 16:11:20 -0300

    Verifies npm package tarball authenticity and integrity (#64)

--- lib/esbuild.ex
+++ lib/esbuild.ex
@@ -222,17 +222,16 @@ defmodule Esbuild do
         freshdir_p(Path.join(System.tmp_dir!(), "phx-esbuild")) ||
         raise "could not install esbuild. Set MIX_XGD=1 and then set XDG_CACHE_HOME to the path you want to use as cache"
 
-    url =
+    name =
       if Version.compare(version, "0.16.0") in [:eq, :gt] do
         target = target()
-        "https://registry.npmjs.org/@esbuild/#{target}/-/#{target}-#{version}.tgz"
+        "@esbuild/#{target}"
       else
         # TODO: Remove else clause or raise if esbuild < 0.16.0 don't need to be supported anymore
-        name = "esbuild-#{target_legacy()}"
-        "https://registry.npmjs.org/#{name}/-/#{name}-#{version}.tgz"
+        "esbuild-#{target_legacy()}"
       end
 
-    tar = fetch_body!(url)
+    tar = Esbuild.NpmRegistry.fetch_package!(name, version)
 
     case :erl_tar.extract({:binary, tar}, [:compressed, cwd: to_charlist(tmp_dir)]) do
       :ok -> :ok
@@ -316,81 +315,5 @@ defmodule Esbuild do
           _ -> raise "esbuild is not available for architecture: #{arch_str}"
         end
     end
-  end
-
-  defp fetch_body!(url) do
-    scheme = URI.parse(url).scheme
-    url = String.to_charlist(url)
-    Logger.debug("Downloading esbuild from #{url}")
-
-    {:ok, _} = Application.ensure_all_started(:inets)
-    {:ok, _} = Application.ensure_all_started(:ssl)
-
-    if proxy = proxy_for_scheme(scheme) do
-      %{host: host, port: port} = URI.parse(proxy)
-      Logger.debug("Using #{String.upcase(scheme)}_PROXY: #{proxy}")
-      set_option = if "https" == scheme, do: :https_proxy, else: :proxy
-      :httpc.set_options([{set_option, {{String.to_charlist(host), port}, []}}])
-    end
-
-    # https://erlef.github.io/security-wg/secure_coding_and_deployment_hardening/inets
-    cacertfile = cacertfile() |> String.to_charlist()
-
-    http_options =
-      [
-        ssl: [
-          verify: :verify_peer,
-          cacertfile: cacertfile,
-          depth: 2,
-          customize_hostname_check: [
-            match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
-          ]
-        ]
-      ]
-      |> maybe_add_proxy_auth(scheme)
-
-    options = [body_format: :binary]
-
-    case :httpc.request(:get, {url, []}, http_options, options) do
-      {:ok, {{_, 200, _}, _headers, body}} ->
-        body
-
-      other ->
-        raise """
-        couldn't fetch #{url}: #{inspect(other)}
-
-        You may also install the "esbuild" executable manually, \
-        see the docs: https://hexdocs.pm/esbuild
-        """
-    end
-  end
-
-  defp proxy_for_scheme("http") do
-    System.get_env("HTTP_PROXY") || System.get_env("http_proxy")
-  end
-
-  defp proxy_for_scheme("https") do
-    System.get_env("HTTPS_PROXY") || System.get_env("https_proxy")
-  end
-
-  defp maybe_add_proxy_auth(http_options, scheme) do
-    case proxy_auth(scheme) do
-      nil -> http_options
-      auth -> [{:proxy_auth, auth} | http_options]
-    end
-  end
-
-  defp proxy_auth(scheme) do
-    with proxy when is_binary(proxy) <- proxy_for_scheme(scheme),
-         %{userinfo: userinfo} when is_binary(userinfo) <- URI.parse(proxy),
-         [username, password] <- String.split(userinfo, ":") do
-      {String.to_charlist(username), String.to_charlist(password)}
-    else
-      _ -> nil
-    end
-  end
-
-  defp cacertfile() do
-    Application.get_env(:esbuild, :cacerts_path) || CAStore.file_path()
   end
 end
---
+++ lib/esbuild/npm_registry.ex
@@ -0,0 +1,145 @@
+defmodule Esbuild.NpmRegistry do
+  @moduledoc false
+  require Logger
+
+  @base_url "https://registry.npmjs.org"
+  @external_resource "lib/esbuild/npm-registry.pem"
+  @public_key_pem File.read!("lib/esbuild/npm-registry.pem")
+  @public_key_id "SHA256:jl3bwswu80PjjokCgh0o2w5c2U4LhQAE57gj9cz1kzA"
+  @public_key_ec_curve :prime256v1
+
+  def fetch_package!(name, version) do
+    %{
+      "_id" => id,
+      "dist" => %{
+        "integrity" => integrity,
+        "signatures" => [
+          %{
+            "keyid" => @public_key_id,
+            "sig" => signature
+          }
+        ],
+        "tarball" => tarball
+      }
+    } =
+      fetch_file!("#{@base_url}/#{name}/#{version}")
+      |> Jason.decode!()
+
+    verify_signature!("#{id}:#{integrity}", signature)
+    tar = fetch_file!(tarball)
+
+    [hash_alg, checksum] =
+      integrity
+      |> String.split("-")
+
+    verify_integrity!(tar, hash_alg, Base.decode64!(checksum))
+
+    tar
+  end
+
+  defp fetch_file!(url) do
+    scheme = URI.parse(url).scheme
+    Logger.debug("Downloading esbuild from #{url}")
+
+    {:ok, _} = Application.ensure_all_started(:inets)
+    {:ok, _} = Application.ensure_all_started(:ssl)
+
+    if proxy = proxy_for_scheme(scheme) do
+      %{host: host, port: port} = URI.parse(proxy)
+      Logger.debug("Using #{String.upcase(scheme)}_PROXY: #{proxy}")
+      set_option = if "https" == scheme, do: :https_proxy, else: :proxy
+      :httpc.set_options([{set_option, {{String.to_charlist(host), port}, []}}])
+    end
+
+    case do_fetch(url) do
+      {:ok, {{_, 200, _}, _headers, body}} ->
+        body
+
+      other ->
+        raise """
+        couldn't fetch #{url}: #{inspect(other)}
+
+        You may also install the "esbuild" executable manually, \
+        see the docs: https://hexdocs.pm/esbuild
+        """
+    end
+  end
+
+  defp do_fetch(url) do
+    scheme = URI.parse(url).scheme
+    url = String.to_charlist(url)
+
+    :httpc.request(
+      :get,
+      {url, []},
+      [
+        ssl: [
+          verify: :verify_peer,
+          # https://erlef.github.io/security-wg/secure_coding_and_deployment_hardening/inets
+          cacertfile: cacertfile() |> String.to_charlist(),
+          depth: 2,
+          customize_hostname_check: [
+            match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
+          ]
+        ]
+      ]
+      |> maybe_add_proxy_auth(scheme),
+      body_format: :binary
+    )
+  end
+
+  defp proxy_for_scheme("http") do
+    System.get_env("HTTP_PROXY") || System.get_env("http_proxy")
+  end
+
+  defp proxy_for_scheme("https") do
+    System.get_env("HTTPS_PROXY") || System.get_env("https_proxy")
+  end
+
+  defp maybe_add_proxy_auth(http_options, scheme) do
+    case proxy_auth(scheme) do
+      nil -> http_options
+      auth -> [{:proxy_auth, auth} | http_options]
+    end
+  end
+
+  defp proxy_auth(scheme) do
+    with proxy when is_binary(proxy) <- proxy_for_scheme(scheme),
+         %{userinfo: userinfo} when is_binary(userinfo) <- URI.parse(proxy),
+         [username, password] <- String.split(userinfo, ":") do
+      {String.to_charlist(username), String.to_charlist(password)}
+    else
+      _ -> nil
+    end
+  end
+
+  defp cacertfile() do
+    Application.get_env(:esbuild, :cacerts_path) || CAStore.file_path()
+  end
+
+  defp verify_signature!(message, signature) do
+    :crypto.verify(
+      :ecdsa,
+      :sha256,
+      message,
+      Base.decode64!(signature),
+      [public_key(), @public_key_ec_curve]
+    ) || raise "invalid signature"
+  end
+
+  defp verify_integrity!(binary, hash_alg, checksum) do
+    hash_alg
+    |> hash_alg_to_erlang()
+    |> :crypto.hash(binary)
+    |> :crypto.hash_equals(checksum) || raise "invalid checksum"
+  end
+
+  defp public_key do
+    [entry] = :public_key.pem_decode(@public_key_pem)
+    {{:ECPoint, ec_point}, _} = :public_key.pem_entry_decode(entry)
+
+    ec_point
+  end
+
+  defp hash_alg_to_erlang("sha512"), do: :sha512
+end
